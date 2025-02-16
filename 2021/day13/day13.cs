03ace25eb6a255c97ea5b04304faa4d8cbf5d775
Vortigon <111313459+Vortigon@users.noreply.github.com>
2025-02-17 00:25:25 +0700

    2021/day13 part 2

--- 2021/day13/day13.cs
+++ 2021/day13/day13.cs
@@ -1,76 +1,79 @@
-using System;
-using System.Numerics;
-using System.IO;
+using System.Text;
 
 namespace aoc_day13
 {
 	class Program
 	{
-
-		class DotsEquailtyComparer : IEqualityComparer<int[]>
-		{
-			public bool Equals(int[]? value1, int[]? value2)
-			{
-				if (value1 == null || value2 == null) return false;
-				return value1[0] == value2[0] && value1[1] == value2[1];
-			}
-			public int GetHashCode(int[] value)
-			{
-				int hash = 23 * 31 + value[0];
-				hash = hash * 31 + value[1];
-				return hash;
-			}
-		}
 		static void Main()
 		{
-			HashSet<int[]> dots = new HashSet<int[]>(new DotsEquailtyComparer());
-			//LinkedList<int[]> dots = new LinkedList<int[]>();
+			HashSet<(int x, int y)> dots = new HashSet<(int x, int y)>();
 			StreamReader input = new("input.txt");
-			while(true)
+			while (true)
 			{
-                string read_input = input.ReadLine();
+				string read_input = input.ReadLine();
 				if (read_input == "") { break; }
 				string[] read_coords = read_input.Split(',');
-
-                int[] coords = new int[2];
-                for (int i = 0; i < read_coords.Length; ++i)
-                {
-					coords[i] = Int32.Parse(read_coords[i]);
-                }
-				dots.Add(coords);
+				int x = int.Parse(read_coords[0]);
+				int y = int.Parse(read_coords[1]);
+				dots.Add((x, y));
 			}
 
-			string folding = input.ReadLine().Replace("fold along ", "");
-			char fold_axis = folding[0];
-			int fold_coord = Int32.Parse(folding.Replace(folding.Substring(0, 2), ""));
-			Console.WriteLine(fold_axis);
-            Console.WriteLine(fold_coord);
-			Console.WriteLine(dots.Count);
+			while (true)
+			{
+				string? folding = input.ReadLine()?.Replace("fold along ", "");
+				if (folding == null) { break; }
+				char fold_axis = folding[0];
+				int fold_coord = int.Parse(folding.Replace(folding.Substring(0, 2), ""));
 
-            fold(dots, fold_axis, fold_coord);
-			Console.WriteLine("Total dots after first fold: " + dots.Count);
-        }
 
-		static void fold(HashSet<int[]> dots, char axis, int fold_coord)
-		{
-			int folding_coord = axis == 'x' ? 0 : 1;
-            foreach (var dot in dots)
-            {
-                if (dot[folding_coord] > fold_coord)
+				dots = fold(dots, fold_axis, fold_coord);
+
+			}
+			input.Close();
+
+			int columns = 0;
+			int rows = 0;
+			foreach (var dot in dots)
+			{
+				columns = int.Max(columns, dot.x);
+				rows = int.Max(rows, dot.y);
+			}
+			StreamWriter output = new StreamWriter("output.txt");
+			for (int i = 0; i <= rows; i++)
+			{
+				var output_string = new StringBuilder(new String('.', columns + 1));
+				foreach (var dot in dots)
 				{
-					
-					int[] new_dot = new int[] { 0,0 };
-					dot.CopyTo(new_dot, 0);
-					new_dot[folding_coord] = dot[folding_coord] - 2 * (dot[folding_coord] - fold_coord);
-
-                    if (dots.Contains(new_dot)) {
-						dots.Remove(dot); 
-					}
-					else {
-						dot[folding_coord] = new_dot[folding_coord]; 
+					if (dot.y == i)
+					{
+						output_string.Replace('.', '#', dot.x, 1);
 					}
 				}
-            }
-        }
+				output.WriteLine(output_string);
+			}
+			output.Close();
+		}
+
+		static HashSet<(int x, int y)> fold(HashSet<(int x, int y)> dots, char axis, int fold_coord)
+		{
+			HashSet<(int x, int y)> new_set = new HashSet<(int x, int y)>();
+			if (axis == 'x')
+			{
+				foreach (var dot in dots)
+				{
+					int new_x = dot.x > fold_coord ? fold_coord - (dot.x - fold_coord) : dot.x;
+					new_set.Add((new_x, dot.y));
+				}
+			}
+			else
+			{
+				foreach (var dot in dots)
+				{
+					int new_y = dot.y > fold_coord ? fold_coord - (dot.y - fold_coord) : dot.y;
+					new_set.Add((dot.x, new_y));
+				}
+			}
+			return new_set;
+		}
 	}
 }
